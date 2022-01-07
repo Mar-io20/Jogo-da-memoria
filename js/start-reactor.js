168ee78203b98c8d975599c3df361815b980a077
Mario Gabriel PC <mario.contate.me@gmail.com>
2022-01-07 09:43:18 -0300

    criando a logica

---
+++ js/start-reactor.js
@@ -0,0 +1,14 @@
+startReactor = {
+    //criando arrey e trabalhando com audio 
+
+    computerCombination: [],
+    playerCombination: [],
+    computerCobinationPosition: 1,
+    combinationMaxPosition: 5, 
+    memoryMaxCombination: 9, 
+
+    audio: {
+        start: '',
+    }
+
+}
